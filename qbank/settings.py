cb548481af335245e22e39f66d4841383217a896
Mayowa Falade <falademayowa240@gmail.com>
2017-07-11 09:17:01 +0100

    improve look and feel

--- qbank/settings.py
+++ qbank/settings.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'qbank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'), 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 AUTHENTICATION_BACKENDS = (
