9e4ce63cb61a7b79991630b27f2d13b1dd26c3f2
Mayowa Falade <falademayowa240@gmail.com>
2017-07-14 23:45:13 +0100

    Improve login page layout.

--- qbank/settings.py
+++ qbank/settings.py
@@ -88,11 +88,14 @@ WSGI_APPLICATION = 'qbank.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('QBANK_DB_NAME'),                      
+        'USER': os.environ.get('QBANK_DB_USER'), 
+        'PASSWORD': os.environ.get('QBANK_DB_PASSWORD'), 
+        'HOST': os.environ.get('QBANK_DB_HOST'),
+        'PORT': os.environ.get('QBANK_DB_PORT'),
     }
 }
 
