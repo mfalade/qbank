52934c8e3758a8fcf0bbda32f53ff1f5627cad69
Mayowa Falade <falademayowa240@gmail.com>
2017-07-15 00:47:46 +0100

    Add whitenoise to enable serving of static files

--- qbank/settings.py
+++ qbank/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 AUTHENTICATION_BACKENDS = (
