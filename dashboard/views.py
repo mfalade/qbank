fc269e592ea00f5c424f2282f31c87529e14a7cb
Mayowa Falade <falademayowa240@gmail.com>
2017-07-17 09:20:02 +0100

    Switch to using ListView for the dashboard accounts list. Implement pagination

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-from django.views import View
 from django.core.paginator import Paginator
+from django.views.generic.list import ListView
 
 from accounts.models import AccountModel
 
 
-# Create your views here.
-class Dashboard(View):
+class DashboardView(ListView):
     template_name = 'dashboard.html'
-    items_limit = 15
+    model = AccountModel
+    context_object_name = 'accounts'
+    paginate_by = 10
     
-    def get(self, request):
-        accounts_qs = AccountModel.objects.order_by('-date_created')
-        paginator = Paginator(accounts_qs, self.items_limit)
-        accounts = paginator.page(1)
-        context = {'accounts': accounts}
-        return render(request, self.template_name, context)
+    def get_context_data(self, **kwargs):
+        return super(DashboardView, self).get_context_data(**kwargs)
--- qbank/urls.py
+++ qbank/urls.py
@@ -17,12 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 
-from dashboard.views import Dashboard
+from dashboard.views import DashboardView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls', namespace='account')),
-    url(r'^dashboard$', login_required(Dashboard.as_view()), name='dashboard'),
+    url(r'^dashboard$', login_required(DashboardView.as_view()), name='dashboard'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url('', include('authentication.urls', namespace='auth')),
 ]
